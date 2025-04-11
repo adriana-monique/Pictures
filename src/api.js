a020f98f0ea3deca47e9b25919e0541cc58f9db3
adriana-monique <perezluevanoadriana1@gmail.com>
2025-04-10 18:55:46 -0700

    asdf

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const searchImages = async(term) => {
-    const url = 'https://api.unsplash.com/search/photos/?client_id='
+    const url = 'https://api.unsplash.com/search/photos/?client_id=UcRCMUe-0_0WNc49l3HKbl-yqujCEk1LDX0ytVyWoyU'
     const res = await axios.get(url,{
         params:{
             query: term
